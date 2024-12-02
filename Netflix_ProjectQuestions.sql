3adbbf88b2d5f592157199526515b16c565ef9c1
Abdullahi Faysal Abdi <am5318317@gmail.com>
2024-12-02 18:27:30 +0300

    Create Netflix_ProjectQuestions.sql

---
+++ Netflix_ProjectQuestions.sql
@@ -0,0 +1,19 @@
+-- 15 Business Problems and solutions 
+
+1. Retrieve the total count of movies and TV shows in the dataset.
+2. Find the most common rating given to movies and TV shows.
+3. List all movies released in the year 2021.
+4. Identify the top 5 countries with the highest number of content available on Netflix.
+5. Determine the movie with the longest duration in the dataset.
+6. Retrieve all content added to Netflix in the last 3 years.
+7. List all movies and TV shows directed by a specific director, such as Martin Scorsese.
+8. Find all TV shows with more than 4 seasons.
+9. Count the number of content items in each genre category.
+10. Calculate the average number of content releases per year in the dataset.
+11. List all movies that are classified as documentaries.
+12. Identify all content without a specified director.
+13. Find the total number of movies actor 'Leonadro DiCprio' has appeared in.
+14. Determine the top 5 actors who have appeared in the most movies in the dataset.
+15. Categorize the content based on the presence of the keywords 'detective' and 'investigation' in the description field 
+    Label content containing either of these keywords as 'Investigative Film' and all other content as 'General' 
+    Determine the count of items falling into each category*/
