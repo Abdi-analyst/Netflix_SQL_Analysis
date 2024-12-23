8478ad404fd27601f26390b01c0cb00ae1c873da
Abdullahi Faysal Abdi <am5318317@gmail.com>
2024-12-23 17:14:49 +0300

    Update Main.sql

--- Main.sql
+++ Main.sql
@@ -1,2 +1,7 @@
 *.sql linguist-language=SQL
 
+*.sql linguist-detectable=true
+*.html linguist-detectable=false
+*.css linguist-detectable=false
+*.js linguist-detectable=false
+*.python linguist-detectable=false
